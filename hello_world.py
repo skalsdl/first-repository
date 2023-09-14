d22f05e6e1a68446a35aebecce1e4cb8e5d250e3
skalsdl <namin3477@naver.com>
2023-09-14 15:32:36 +0900

    gekko

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
 def print_hello():
-    print("Hello World!!")
+    print("gekko!!")
 
 if __name__ == '__main__':
     print_hello()
--- prime_number.py
+++ prime_number.py
@@ -3,5 +3,5 @@ def is_prime_number(x):
         if x % num == 0:
             print("The number {num1} is not a prime number.".format(num1=x))
             return
-    print("The number {num1} is a prime number.".format(num1=x))
+    print("The number {num1} is a prime number!!".format(num1=x))
 
